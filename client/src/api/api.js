53628b6fc36681f54d34d59d3cd9013dba939c2d
RoniBarda <roniba18@walla.co.il>
2025-03-11 11:47:19 +0200

    ...

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
+const baseURL = Process.env.NODE_ENV === 'production' ? '/api' : 'http://localhost:3000/api';
+
 export default axios.create({
-  baseURL: "http://localhost:3000/api",
-  headers: { "Content-type": "application/json" },
+  baseURL,
+  headers: {
+    "Content-type": "application/json"
+  },
   withCredentials: true,
 });
--- server/index.js
+++ server/index.js
@@ -3,14 +3,27 @@ const routes = require("./routes/index");
 const connectDB = require("./lib/connect");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const path = require('path');
 
 const app = express();
-app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: ["http://localhost:5173"], credentials: true }));
+app.use(express.json());
+app.use(
+  cors({
+    origin: ["http://localhost:5173", 'https://oy-vay-pay.onrender.com'],
+    credentials: true,
+  })
+);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use("/api", routes);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.listen(3000, () => {
   connectDB();
-  console.log("Server is running on http://localhost:3000/api");
+  console.log("Server is running on http://localhost:3000");
 });
